a63ad859d7822a34f6a5f5adf2f7e6e8f4d11ab6
Kayro Mizael <kayro@interativadigital.com.br>
2020-02-03 15:27:34 -0300

    Updated cors, utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const http = require('http');
+
+const {setupWebsocket} = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-zuo2o.mongodb.net/omnistack10?retryWrites=true&w=majority', {
@@ -10,6 +17,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-zuo2o.mongodb.net/o
 	useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
